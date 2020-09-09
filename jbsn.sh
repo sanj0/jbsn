82f2e348671baa2ea32bdd519bc6d02fea330936
Malte <malte.dostal@gmail.com>
2020-09-09 17:23:56 +0200

    enhance run script to update git before and after starting jbsn

--- jbsn.sh
+++ jbsn.sh
@@ -3,12 +3,16 @@ $DIR=${pwd}
 # check if default jbsn configs and notes
 # dir is a git repo and if so, pull and then push
 # to update local and remote before starting the client
-if [[ -d ~/jbsn/.git/ ]]; then
-    echo "detected git repo at default location, updating..."
-    cd ~/jbsn/
-    git add .
-    git commit -m "automated commit at jbsn startup"
-    git pull
-    git push
-fi
+function syncGit {
+    if [[ -d ~/jbsn/.git/ ]]; then
+        echo "detected git repo at default location, updating..."
+        cd ~/jbsn/
+        git add .
+        git commit -m "automated commit at jbsn startup"
+        git pull
+        git push
+    fi
+}
+syncGit
 java -cp ~/jbsn/bin/jbsn.jar de.edgelord.jbsn.Main
+syncGit
